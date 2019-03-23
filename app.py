96a70f5a1eec8e1d688a6cfd6b08318a8ad9c891
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-03-23 15:11:06 +0530

    Added the route for the fulfillment of the dialogflow after completion of the intent

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request, make_response
 
 app = Flask(__name__)
 
 
-@app.route('/respond', methods=['GET'])
-def respond():
+@app.route('/', methods=['GET'])
+def index():
     return jsonify({"name": "JV"})
 
 
 @app.route('/respond', methods=['POST'])
 def respond_post():
-    return request.data
-
+    res = (request.get_json(force=True)) #Get the json from dialogflow
+    print(jsonify(res))                 # To convert into json
+    return make_response(jsonify({"fulfillmentText":"hello from webhook"}))
 
 if(__name__ == "__main__"):
     app.run(port=8080, debug=True)
